95f39b106d0b349a80e09357ccc14375a1d71e49
Enmanuel <enmanuellagares@gmail.com>
2024-04-04 15:04:59 -0400

    replace: copilot => codeium

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,13 +3,27 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  'github/copilot.vim',
+  { 'github/copilot.vim', enabled = false },
+  {
+    'Exafunction/codeium.nvim',
+    -- dependencies = {
+    --   'nvim-lua/plenary.nvim',
+    --   'hrsh7th/nvim-cmp',
+    -- },
+    config = function()
+      require('codeium').setup {}
+    end,
+  },
   {
     'stevearc/oil.nvim',
     opts = {},
     config = function()
       local oil = require 'oil'
-      oil.setup()
+      oil.setup {
+        view_options = {
+          show_hidden = true,
+        },
+      }
       vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
       vim.keymap.set('n', '<leader>-', oil.toggle_float, { desc = 'Toggle parent directory in floating window' })
     end,
